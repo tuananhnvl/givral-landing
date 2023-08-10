ee7f4b6aaf95363eb8c1f6042f6bda1ac3922781
tuananhnvl <tuananh.namvietluat@gmail.com>
2023-08-10 12:39:36 +0700

    up

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,6 @@
 import '@/styles/globals.css'
 import '@/styles/main.css'
-import { LenisProvider } from './LenisProvider.js'
+import { LenisProvider } from './provider/LenisProvider.js'
 export default function App({ Component, pageProps }) {
   return    <LenisProvider>
     
